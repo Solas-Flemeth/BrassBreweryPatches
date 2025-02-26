59c274d3a56e96a54d15cf1b4fb0b57dc5d71ec7
sanforjr2021 <jacobsanford2017@gmail.com>
2025-02-26 00:40:39 -0500

    Initial Commit

---
+++ BrassBreweryPatches/BrassBreweryPatches/BrassBreweryPatchesModSystem.cs
@@ -0,0 +1,27 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+
+namespace BrassBreweryPatches;
+
+public class BrassBreweryPatchesModSystem : ModSystem
+{
+    // Called on server and client
+    // Useful for registering block/entity classes on both sides
+    public override void Start(ICoreAPI api)
+    {
+        Mod.Logger.Notification("Loading "+ Mod.Info.Name + " on " + api.Side);
+        
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+
+    }
+}
